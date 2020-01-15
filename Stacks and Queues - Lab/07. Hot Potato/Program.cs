4ea350894c0b58135186d2064a62db34ff5b1846
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 21:14:50 +0200

    HotPotato

---
+++ Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int toss = int.Parse(Console.ReadLine());
+
+            Queue<string> childrens = new Queue<string>(input);
+
+            int count = 1;
+
+            while (childrens.Count != 1)
+            {
+                string child = childrens.Dequeue();
+
+                if (count == toss)
+                {
+                    Console.WriteLine($"Removed {child}");
+                    count = 0;
+                }
+                else
+                {
+                    childrens.Enqueue(child);
+                }
+
+                count++;
+            }
+
+            Console.WriteLine($"Last is {childrens.Dequeue()}");
+        }
+    }
+}
