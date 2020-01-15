736e9280c5c05144783d3df9cc64955ab7a1ff37
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 14:16:52 +0200

    Simple Calculator

---
+++ Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> expression = new Stack<string>(input.Reverse());
+
+            while (expression.Count > 1)
+            {
+                int firstNumber = int.Parse(expression.Pop());
+                string operation = expression.Pop();
+                int secondNumber = int.Parse(expression.Pop());
+
+                int result = operation switch
+                {
+                    "+" => (firstNumber + secondNumber),
+                    "-" => (firstNumber - secondNumber),
+                };
+
+                expression.Push(result.ToString());
+            }
+
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
