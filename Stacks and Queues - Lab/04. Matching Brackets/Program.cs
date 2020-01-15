d914fd3ae62a8fb5df58a64fea6ab8bff744ab9b
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 14:42:32 +0200

    Matching Brackets

---
+++ Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if (symbol == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (symbol == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int lenght = i - startIndex + 1;
+                    string expresion = input.Substring(startIndex, lenght);
+
+                    Console.WriteLine(expresion);
+                }
+            }
+        }
+    }
+}
