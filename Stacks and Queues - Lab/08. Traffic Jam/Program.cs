bb5757d0d7994d6b61c34c84b7bfb9f7ac5ceef0
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 21:28:18 +0200

    Traffic Jam

---
+++ Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> cars = new Queue<string>();
+            int totalCars = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n && cars.Any(); i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        totalCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalCars} cars passed the crossroads.");
+        }
+    }
+}
