099089536e9c013a398390d23973eb36d0aa6eaa
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 13:39:40 +0200

    Stack Sum

---
+++ Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> elements = new Stack<int>(numbers);
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().ToLower().Split();
+                string commandArguments = command[0];
+
+                if (commandArguments == "add")
+                {
+                    elements.Push(int.Parse(command[1]));
+                    elements.Push(int.Parse(command[2]));
+                }
+                else if (commandArguments == "remove")
+                {
+                    int numbersForRemove = int.Parse(command[1]);
+                    if (elements.Count >= numbersForRemove)
+                    {
+                        for (int i = 0; i < numbersForRemove; i++)
+                        {
+                            elements.Pop();
+                        }
+                    }
+                }
+                else if (commandArguments == "end")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {elements.Sum()}");
+        }
+    }
+}
