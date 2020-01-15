6273eff0674a71195b373ac6e3206eb41b48858d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-15 21:00:37 +0200

    Supermarket

---
+++ Stacks and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> people = new Queue<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (people.Any())
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else
+                {
+                    people.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
