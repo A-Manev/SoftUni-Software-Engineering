3f46c855a35a8803603657bcb4f231670d1edbc5
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-16 21:31:26 +0200

    Fashion Boutique

---
+++ Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes = new Stack<int>(input);
+
+            int racksCount = 1;
+            int capacity = rackCapacity;
+
+            while (clothes.Any())
+            {
+                int item = clothes.Peek();
+
+                if (capacity - item >= 0)
+                {
+                    capacity -= item;
+                    clothes.Pop();
+                }
+                else
+                {
+                    racksCount++;
+                    capacity = rackCapacity;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
