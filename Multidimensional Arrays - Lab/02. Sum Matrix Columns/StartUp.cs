ad9b6332ee5cf493c42ebec94369e59bc043bd7e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-19 21:48:56 +0200

    Sum Matrix Columns

---
+++ Multidimensional Arrays - Lab/02. Sum Matrix Columns/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            int[] colsSum = new int[cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    colsSum[col] += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, colsSum));
+        }
+    }
+}
