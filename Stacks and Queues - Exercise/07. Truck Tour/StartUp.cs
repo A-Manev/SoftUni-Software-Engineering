4e097baeb914e64fbfd0508ef2cf4dfadea12915
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-19 18:07:25 +0200

    Truck Tour

---
+++ Stacks and Queues - Exercise/07. Truck Tour/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int pumpsCount = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            FillQueue(pumpsCount, pumps);
+
+            int startIndex = 0;
+
+            while (true)
+            {
+                int fuelAmount = 0;
+                bool startPoint = true;
+
+                foreach (var pump in pumps)
+                {
+                    fuelAmount += pump[0]; 
+
+                    if (fuelAmount < pump[1])
+                    {
+                        startPoint = false;
+                        break;
+                    }
+
+                    fuelAmount -= pump[1];
+                }
+
+                if (startPoint)
+                {
+                    break;
+                }
+
+                startIndex++;
+
+                pumps.Enqueue(pumps.Dequeue());
+            }
+
+            Console.WriteLine(startIndex);
+        }
+
+        private static void FillQueue(int pumpsCount, Queue<int[]> pumps)
+        {
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                int[] currentPump = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                pumps.Enqueue(currentPump);
+            }
+        }
+    }
+}
