cd9f5a6aca5df3d80b93a9df93bc07d19348db62
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-19 16:05:22 +0200

    Cups and Bottles

---
+++ Stacks and Queues - Exercise/12. Cups and Bottles/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CupsAndBottles
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] cupsCapacity = Console.ReadLine()
+                  .Split()
+                  .Select(int.Parse)
+                  .ToArray();
+            int[] waterBottles = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> bottles = new Stack<int>(waterBottles);
+            Stack<int> cups = new Stack<int>(cupsCapacity.Reverse());
+
+            int wastedWater = 0;
+
+            while (bottles.Any() && cups.Any())
+            {
+                int bottle = bottles.Pop();
+                int cup = cups.Peek();
+
+                if (cup <= bottle)
+                {
+                    wastedWater += bottle - cup;
+                    cups.Pop();
+                }
+                else
+                {
+                    cups.Pop();
+                    cups.Push(cup - bottle);
+                }
+            }
+
+            if (bottles.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
