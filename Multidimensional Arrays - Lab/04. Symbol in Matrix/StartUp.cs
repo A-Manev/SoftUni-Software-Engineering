e9b91fc800273e91ba69d6fedcad80ad7b1639fc
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-20 11:06:43 +0200

    Symbol in Matrix

---
+++ Multidimensional Arrays - Lab/04. Symbol in Matrix/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SymbolInMatrix
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int squareMatrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[squareMatrixSize, squareMatrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    { 
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
