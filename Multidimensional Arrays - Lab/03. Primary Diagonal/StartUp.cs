9f59cd2f33ca10fbca81fcc40337731f230fcbdb
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-20 11:05:47 +0200

    Primary Diagonal

---
+++ Multidimensional Arrays - Lab/03. Primary Diagonal/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int squareMatrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[squareMatrixSize, squareMatrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int diagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    diagonalSum += matrix[row, col];
+                    row++;
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
