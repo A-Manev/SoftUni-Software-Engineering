cf3fb63455d3a10069852e73912250b00eee2392
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-20 13:54:35 +0200

    Jagged-Array Modification

---
+++ Multidimensional Arrays - Lab/06. Jagged-Array Modification/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayModification
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                matrix[row] = input;
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] cmdArg = command.Split().ToArray();
+                int row = int.Parse(cmdArg[1]);
+                int col = int.Parse(cmdArg[2]);
+                int value = int.Parse(cmdArg[3]);
+
+                if (command.StartsWith("Add "))
+                {
+                    if (IsInside(matrix, row, col))
+                    {
+                        matrix[row][col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                else if (command.StartsWith("Subtract "))
+                {
+                    if (IsInside(matrix, row, col))
+                    {
+                        matrix[row][col] -= value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+
+        private static bool IsInside(int[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length &&
+                   col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
