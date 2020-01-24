621f7208181f020f8b6e060af625de6da85cadbd
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-24 22:30:44 +0200

    Cities by Continent and Country

---
+++ Sets and Dictionaries Advanced - Lab/04. Cities by Continent and Country/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Dictionary<string, Dictionary<string, List<string>>> records = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string continentName = command[0];
+                string countryName = command[1];
+                string cityName = command[2];
+
+                if (!records.ContainsKey(continentName))
+                {
+                    records.Add(continentName, new Dictionary<string, List<string>>());
+                }
+
+                if (!records[continentName].ContainsKey(countryName))
+                {
+                    records[continentName].Add(countryName, new List<string>());
+                }
+
+                records[continentName][countryName].Add(cityName);
+            }
+
+            foreach (var continent in records)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var (country, city) in continent.Value)
+                {
+                    Console.WriteLine($"  {country} -> {string.Join(", ", city)}");
+                }
+            }
+        }
+    }
+}
