37bcdffad5930c552c22a4ad225531cd8803a921
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-24 22:31:10 +0200

    Record Unique Names

---
+++ Sets and Dictionaries Advanced - Lab/05. Record Unique Names/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+
+                names.Add(command);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
