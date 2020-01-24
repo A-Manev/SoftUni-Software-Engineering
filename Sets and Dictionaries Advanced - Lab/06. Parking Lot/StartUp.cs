3daafa4844faa326341b7a926bebe19e4f068f56
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-24 22:32:10 +0200

    Parking Lot

---
+++ Sets and Dictionaries Advanced - Lab/06. Parking Lot/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] commandArguments = command
+                    .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string direction = commandArguments[0];
+                string carNumber = commandArguments[1];
+
+                if (direction == "IN")
+                {
+                    carNumbers.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    carNumbers.Remove(carNumber);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (carNumbers.Any())
+            {
+                foreach (var number in carNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
