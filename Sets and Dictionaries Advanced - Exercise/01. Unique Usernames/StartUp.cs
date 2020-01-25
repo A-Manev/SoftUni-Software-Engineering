0908055d335b0a4a4b9da19f79aca518b7dfb959
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-25 22:16:53 +0200

    Unique Usernames

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            HashSet<string> usernames = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputUsername = Console.ReadLine();
+
+                usernames.Add(inputUsername);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
