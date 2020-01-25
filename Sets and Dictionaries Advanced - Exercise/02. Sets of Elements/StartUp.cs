298b57aaebd97f128a16068a99900ee18b9e1fa4
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-25 22:17:22 +0200

    Sets of Elements

---
+++ Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SetsOfElements
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            int[] input = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int n = input[0];
+            int m = input[1];
+
+            for (int i = 0; i < n + m; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    firstSet.Add(inputNumber);
+                }
+                else
+                {
+                    secondSet.Add(inputNumber);
+                }
+            }
+
+            foreach (var numOne in firstSet)
+            {
+                foreach (var numTwo in secondSet)
+                {
+                    if (numOne == numTwo)
+                    {
+                        Console.Write(numOne + " ");
+                    }
+                }
+            }
+        }
+    }
+}
