56150c4780b3253765e8208fdb48a1b9edb1f861
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-25 22:17:56 +0200

    Periodic Table

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            HashSet<string> chemicalElements = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemicalElements.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements.OrderBy(x => x)));
+        }
+    }
+}
