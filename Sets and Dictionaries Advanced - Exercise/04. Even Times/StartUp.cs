ba27d66aba0bf58729f94ff45efc975dcd9b300c
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-25 22:18:34 +0200

    Even Times

---
+++ Sets and Dictionaries Advanced - Exercise/04. Even Times/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(inputNumber))
+                {
+                    numbers.Add(inputNumber , 0);
+                }
+
+                numbers[inputNumber]++;
+            }
+
+            foreach (var num in numbers.Where(x=>x.Value % 2 == 0))
+            {
+                Console.WriteLine(num.Key);
+            }
+        }
+    }
+}
