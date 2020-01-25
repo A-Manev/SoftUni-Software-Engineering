0505ed1ea95f78aa16d88c666cbd507a493ee060
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-25 22:19:13 +0200

    Count Symbols

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols.Add(symbol, 0);
+                }
+
+                symbols[symbol]++;
+            }
+
+            foreach (var (symbol, times) in symbols.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{symbol}: {times} time/s");
+            }
+        }
+    }
+}
