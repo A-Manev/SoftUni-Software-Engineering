1a7301684adf59dcd012e3ad034c6877d70b17f5
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-29 21:56:09 +0200

    Word Count

---
+++ Streams, Files and Directories - Exercise/03. Word Count/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCount
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] inputWords = File
+                .ReadAllLines("./words.txt");
+
+            string[] inputText = File
+                .ReadAllText("./text.txt")
+                .ToLower()
+                .Split(new char[] { ' ', '.', ',', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            int counter = 0;
+
+            foreach (var currentWord in inputWords)
+            {
+                foreach (var word in inputText)
+                {
+                    if (currentWord == word)
+                    {
+                        counter++;
+                    }
+                }
+
+                words[currentWord] = counter;
+                counter = 0;
+            }
+
+            words = words.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var (word, count) in words)
+            {
+                File.AppendAllText("../../../actualResult.txt", $"{word} - {count}{Environment.NewLine}");
+            }
+        }
+    }
+}
