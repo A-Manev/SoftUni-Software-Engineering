c853c87fb5bb69b3d128ed063003d8d34bf3a425
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-29 21:55:30 +0200

    Line Numbers

---
+++ Streams, Files and Directories - Exercise/02. Line Numbers/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] inputLines = File.ReadAllLines("./text.txt");
+            string[] outputLines = new string[inputLines.Length];
+
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                string currentLine = inputLines[i];
+
+                int lettersCount = CountLetters(currentLine);
+                int punctuationMarksCount = CountPunctuationMarks(currentLine);
+
+                outputLines[i] = $"Line {i + 1}: {currentLine} ({lettersCount})({punctuationMarksCount})";
+            }
+
+            File.WriteAllLines("../../../output.txt", outputLines);
+        }
+
+        private static int CountPunctuationMarks(string currentLine)
+        {
+            int punctuationMarksCount = 0;
+            for (int j = 0; j < currentLine.Length; j++)
+            {
+                char currentSymbol = currentLine[j];
+
+                if (char.IsPunctuation(currentSymbol))
+                {
+                    punctuationMarksCount++;
+                }
+            }
+
+            return punctuationMarksCount;
+        }
+
+        private static int CountLetters(string currentLine)
+        {
+            int lettersCount = 0;
+
+            for (int j = 0; j < currentLine.Length; j++)
+            {
+                char currentSymbol = currentLine[j];
+
+                if (char.IsLetter(currentSymbol))
+                {
+                    lettersCount++;
+                }
+            }
+
+            return lettersCount;
+        }
+    }
+}
