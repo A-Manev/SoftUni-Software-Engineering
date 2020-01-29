25634feac08aae716f88fcb7a644e9f1c6649f59
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-29 22:01:28 +0200

    Zip and Extract

---
+++ Streams, Files and Directories - Exercise/06. Zip and Extract/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace ZipAndExtract
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            ZipFile.CreateFromDirectory(@"D:\SoftUni\Files\", @"D:\SoftUni\myArchive.zip");
+
+            ZipFile.ExtractToDirectory(@"D:\SoftUni\myArchive.zip", @"C:\Riot Games\Riot Client\Plugins\rcp-be-agent");
+        }
+    }
+}
+  
