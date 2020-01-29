b8a62f409b82912e0c46f7d96b67d34d279dc01d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-29 21:57:21 +0200

    Copy Binary File

---
+++ Streams, Files and Directories - Exercise/04. Copy Binary File/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            const int DEF_SIZE = 4096;
+
+            string inputPath = "./copyMe.png";
+            string outputPath = "../../../image.png";
+
+            using FileStream reader = new FileStream(inputPath, FileMode.Open);
+            using FileStream writer = new FileStream(outputPath, FileMode.Create);
+
+            byte[] buffer = new byte[DEF_SIZE];
+
+            while (reader.CanRead)
+            {
+                int readBytes = reader.Read(buffer, 0, buffer.Length);
+
+                if (readBytes == 0)
+                {
+                    break;
+                }
+
+                writer.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
