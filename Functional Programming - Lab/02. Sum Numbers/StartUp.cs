22b2b6bdbaa4b26a3a079ac07429fb40fe719a12
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-30 22:50:23 +0200

    Sum Numbers

---
+++ Functional Programming - Lab/02. Sum Numbers/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] inputNumber = Console.ReadLine()
+                 .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Console.WriteLine(inputNumber.Length);
+            Console.WriteLine(inputNumber.Sum());
+        }
+    }
+}
