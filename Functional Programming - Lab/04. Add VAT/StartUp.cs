42c2715ff089445bc510325484df3b1cd81dd4c4
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-30 22:51:51 +0200

    Vat

---
+++ Functional Programming - Lab/04. Add VAT/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            double[] inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x=>x * 1.2)
+                .ToArray();
+
+            foreach (var price in inputNumbers)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
