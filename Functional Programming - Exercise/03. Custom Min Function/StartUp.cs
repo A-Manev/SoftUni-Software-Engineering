ebb8bc5f73873fb25fbdbcad9795892e5f72a8fa
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-31 22:02:55 +0200

    Custom Min Function

---
+++ Functional Programming - Exercise/03. Custom Min Function/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            /// First variations
+            //Func<int[], int> minFunc = x => x.Min();
+            //Console.WriteLine(minFunc(inputNumbers));
+
+            /// Second variations
+            Func<int[], int> minFunc = FindSmallestNumber;
+
+            int minNumber = minFunc(inputNumbers);
+
+            Console.WriteLine(minNumber);
+        }
+
+        /// Second variations
+        private static int FindSmallestNumber(int[] numbers)
+        {
+            int minNumber = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            return minNumber;
+        }
+    }
+}
