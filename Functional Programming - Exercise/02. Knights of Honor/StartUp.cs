eb57cf28d0314a4c09379bb7b09d51cf270c2104
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-31 22:02:14 +0200

    Knights of Honor

---
+++ Functional Programming - Exercise/02. Knights of Honor/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] inputNames = Console.ReadLine()
+                  .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                  .ToArray();
+
+            /// First variations
+            Action<string> appendSir = name => Console.WriteLine($"Sir {name}");
+
+            /// First variations
+            foreach (var name in inputNames)
+            {
+                appendSir(name);
+            }
+
+            /// Second variations
+            //foreach (var name in inputNames)
+            //{
+            //    Action<string> print = Print(name);
+            //    print(name);
+            //}
+        }
+
+        /// Second variations
+        //static Action<string> Print(string name)
+        //{
+        //    return name => Console.WriteLine("Sir " + name);
+        //}
+    }
+}
