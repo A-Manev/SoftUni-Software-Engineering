b683d01522c8e7b32a57d5d6a4b768c056be8e80
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-01-31 22:01:38 +0200

    Action Print

---
+++ Functional Programming - Exercise/01. Action Print/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPrint
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] inputNames = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            /// First variations
+            Action<string[]> print = name => Console.WriteLine(string.Join(Environment.NewLine, name));
+            print(inputNames);
+
+            /// Second variations
+            //Action<string[]> print = Print;
+            //print(inputNames);
+
+            /// Third variations
+            //foreach (var name in inputNames)
+            //{
+            //    Action<string> writer = Writer(name);
+
+            //    writer(name);
+            //}
+        }
+
+        /// Second variations
+        //static void Print(string[] names)
+        //{
+        //    Console.WriteLine(string.Join(Environment.NewLine, names));
+        //}
+
+        /// Third variations
+        //static Action<string> Writer(string name)
+        //{
+        //    return x => Console.WriteLine(x);
+        //}
+    }
+}
