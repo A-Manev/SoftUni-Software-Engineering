533d06503c2b156c92b37ae1e51f74396488e30e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-02 21:47:39 +0200

    Predicate For Names

---
+++ Functional Programming - Exercise/07. Predicate For Names/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PredicateForNames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            Predicate<string> filterByLength  = name => name.Length > length;
+            Action<List<string>> print = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+            names.RemoveAll(filterByLength);
+            print(names);
+        }
+    }
+}
