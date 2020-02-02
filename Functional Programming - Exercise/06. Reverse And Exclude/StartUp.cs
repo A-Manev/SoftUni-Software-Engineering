949901b7da3c7edc5027b78730d98f5cf0583ae5
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-02 21:46:50 +0200

    Reverse And Exclude

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReverseAndExclude
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            List<int> elements = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            int divider = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDevisible = x => x % divider == 0;
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            elements.Reverse();
+            elements.RemoveAll(isDevisible);
+            print(elements);
+        }
+    }
+}
