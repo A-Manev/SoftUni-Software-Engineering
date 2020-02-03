446949ef418134bc80d5c238407bc0d5b17ffd92
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-03 21:22:17 +0200

    Custom Comparator

---
+++ Functional Programming - Exercise/08. Custom Comparator/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Func<int, int, int> comparator = new Func<int, int, int>((a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return a.CompareTo(b);
+                }
+            });
+
+            Comparison<int> comparison = new Comparison<int>(comparator);
+
+            int[] inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(inputNumbers, comparison);
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+        }
+    }
+}
