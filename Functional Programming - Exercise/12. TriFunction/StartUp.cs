3cdb1874bd56faec3f20365bae76dbc630119ab1
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-03 22:21:29 +0200

    TriFunction

---
+++ Functional Programming - Exercise/12. TriFunction/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> func = (str, num) => str.ToCharArray().Select(c => (int)c).Sum() >= num;
+            Func<string[], int, Func<string, int, bool>, string> name = (n, num, f) => names.FirstOrDefault(s => func(s, num));
+
+            Console.WriteLine(name(names, number, func));
+        }
+    }
+}
