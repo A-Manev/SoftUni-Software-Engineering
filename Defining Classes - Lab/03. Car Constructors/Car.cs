7bc54ab9ebb7f55260efcd44a8cfcd51652b20c3
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-04 22:08:22 +0200

    Car Constructors

---
+++ Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,39 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            :this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+    }
+}
