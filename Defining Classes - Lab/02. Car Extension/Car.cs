83519fc75ef86f1a4432e1079e6cd08460e8ebb3
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-04 22:02:05 +0200

    Car Extension

---
+++ Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            bool checkFuel = FuelQuantity - (distance / 100 * FuelConsumption) > 0;
+
+            if (checkFuel)
+            {
+                FuelQuantity -= distance / 100 * FuelConsumption;
+            }
+            else
+            {
+                System.Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
