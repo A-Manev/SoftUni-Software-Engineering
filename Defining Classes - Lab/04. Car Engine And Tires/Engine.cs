5d15b1b2ee169738ecec41a7918c3dc6e799d0f0
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-04 22:11:52 +0200

    Car Engine And Tires

---
+++ Defining Classes - Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,19 @@
+﻿namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double CubicCapacity { get; set; }
+    }
+
+}
---
+++ Defining Classes - Lab/04. Car Engine And Tires/Tire.cs
@@ -0,0 +1,20 @@
+﻿namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+
+        public int Year { get; set; }
+
+        public double Pressure { get; set; }
+    }
+}
+
+
