b70a022df3609392b8e71589fe56af68e3523860
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-05 22:34:13 +0200

    Date Modifier

---
+++ Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DateModifier
+{
+    public long CalculateDifferenceBetweenTwoDatas(List<DateTime> datas)
+    {
+        TimeSpan result = datas[0] - datas[1];
+
+        string data = Data(result);
+
+        string[] currentDays = data.Split(".");
+
+        long days = long.Parse(currentDays[0]);
+
+        return Math.Abs(days);
+    }
+
+    public string Data(TimeSpan result)
+    {
+        string date = result.ToString();
+
+        date = date.Replace("/", "");
+        date = date.Replace(":", "");
+        date = date.Replace(" ", "");
+        date = date.Replace("AM", "");
+        date = date.Replace("PM", "");
+        return date;
+    }
+}
+
---
+++ Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<DateTime> datas = new List<DateTime>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                string[] dataInput = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                int year = int.Parse(dataInput[0]);
+                int month = int.Parse(dataInput[1]);
+                int day = int.Parse(dataInput[2]);
+
+                DateTime data = new DateTime(year, month, day);
+                datas.Add(data);
+            }
+
+            DateModifier dateModifier = new DateModifier();
+
+            long result = dateModifier.CalculateDifferenceBetweenTwoDatas(datas);
+
+            Console.WriteLine(result);
+        }
+    }
+}
