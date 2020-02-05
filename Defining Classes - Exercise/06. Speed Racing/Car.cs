df3628efaba88869783bf329e28249157c4ab681
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-05 22:34:50 +0200

    Speed Racing

---
+++ Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, decimal fuelAmount, decimal fuelConsumptionPerKilometer, decimal travelledDistance = 0)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = travelledDistance;
+        }
+
+        public string Model { get; set; }
+
+        public decimal FuelAmount { get; set; }
+
+        public decimal FuelConsumptionPerKilometer { get; set; }
+
+        public decimal TravelledDistance { get; set; }
+
+        public void Drive(decimal distance)
+        {
+           decimal neededFuel = distance * FuelConsumptionPerKilometer;
+
+            if (neededFuel <= FuelAmount)
+            {
+                FuelAmount -= neededFuel;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            HashSet<Car> cars = new HashSet<Car>();
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] inputCar = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = inputCar[0];
+                decimal fuelAmount = decimal.Parse(inputCar[1]);
+                decimal fuelConsumptionFor1km = decimal.Parse(inputCar[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1km);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandArgumenst = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string carModel = commandArgumenst[1];
+                decimal amountOfKm = decimal.Parse(commandArgumenst[2]);
+
+                var currentCar = cars.FirstOrDefault(x => x.Model == carModel);
+
+                currentCar.Drive(amountOfKm);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+
+        }
+    }
+}
