d1e7cf9ad9c99433743d70a4ab633c1d47b493ac
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-05 22:33:39 +0200

    Opinion Poll

---
+++ Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                if (age > 30)
+                {
+                    Person person = new Person(name, age);
+                    people.Add(person);
+                }
+            }
+
+            foreach (var person in people.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
