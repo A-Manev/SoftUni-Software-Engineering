f2777c22d6e4d95fd262f5e43334f47c57629491
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-05 22:33:04 +0200

    Oldest Family Member

---
+++ Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private  List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+           this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = this.members
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return oldestPerson;
+        }
+    }
+}
---
+++ Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public partial class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person member = new Person(name, age);
+
+                family.AddMember(member);
+            }
+
+            var oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
