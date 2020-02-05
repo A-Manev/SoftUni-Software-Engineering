a73b4845892a674994b56c4f5e4f806505410dee
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-05 22:31:35 +0200

    Define a Class Person

---
+++ Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = "Pesho";
+            int age = 24;
+
+            Person person = new Person()
+            {
+                Name = name,
+                Age = age
+            };
+
+            Console.WriteLine($"{person.Name} -> {person.Age}");
+        }
+    }
+}
