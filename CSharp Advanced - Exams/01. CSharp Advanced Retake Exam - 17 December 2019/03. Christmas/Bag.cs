e034fe33de4025fd36b37fdc15bf7844634c187f
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-10 13:37:32 +0200

    Christmas

---
+++ CSharp Advanced - Exams/01. CSharp Advanced Retake Exam - 17 December 2019/03. Christmas/Bag.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        private List<Present> data;
+
+        public Bag(string color, int capacity)
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+            this.data = new List<Present>();
+        }
+
+        public int Count => this.data.Count;
+
+        public string Color { get; set; }
+
+        public int Capacity { get; set; }
+
+        public void Add(Present present)
+        {
+            if (this.Capacity != 0 && !data.Contains(present))
+            {
+                data.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var target = data.FirstOrDefault(x => x.Name == name);
+
+            if (target == null)
+            {
+                return false;
+            }
+
+            data.Remove(target);
+
+            return true;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            int maxWeight = int.MinValue;
+
+            Present heaviestPresent = null;
+
+            foreach (var present in data)
+            {
+                if (present.Weight > maxWeight)
+                {
+                    heaviestPresent = present;
+                }
+            }
+
+            return heaviestPresent;
+        }
+
+        public Present GetPresent(string name)
+        {
+            Present targetPresent = data.First(x => x.Name == name);
+
+            return targetPresent;
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder
+                .AppendLine($"{this.Color} bag contains:");
+
+            foreach (var present in data)
+            {
+                stringBuilder.AppendLine(present.ToString());
+            }
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp Advanced - Exams/01. CSharp Advanced Retake Exam - 17 December 2019/03. Christmas/Present.cs
@@ -0,0 +1,25 @@
+﻿namespace Christmas
+{
+    public class Present
+    {
+        public Present(string name, double weight, string gender)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"Present {this.Name} ({this.Weight}) for a {this.Gender}";
+
+            return result;
+        }
+    }
+}
