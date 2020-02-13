6cf557a4640786c64bc5b0942d6c29f045c31f52
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-13 11:20:14 +0200

    Box

---
+++ Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> data;
+
+        public Box()
+        {
+            this.data = new Stack<T>();
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(T element)
+        {
+            this.data.Push(element);
+        }
+
+        public T Remove()
+        {
+            return this.data.Pop();
+        }
+    }
+}
---
+++ Generics - Lab/01. Box/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
