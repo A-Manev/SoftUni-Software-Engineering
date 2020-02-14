8f0678bacbf175655658c158f1a1870854f3611c
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-14 11:36:03 +0200

    Generic Box of String

---
+++ Generics - Exercise/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
