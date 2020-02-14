d1f157dcfaa989cbd1e739a2e6c7141fb7753ca4
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-14 11:37:05 +0200

    Generic Swap Method String

---
+++ Generics - Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Add(input);
+            }
+
+            int[] swapIndexes = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            box.Swap(swapIndexes[0], swapIndexes[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
