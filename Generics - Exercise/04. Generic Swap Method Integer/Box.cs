1170030f1272fc90d9ea04ef4f659c8c7a2a6360
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-14 11:37:39 +0200

    Generic Swap Method Integer

---
+++ Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+namespace GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public void Swap(int indexOne, int indexTwo)
+        {
+            T current = this.data[indexTwo];
+            this.data[indexTwo] = this.data[indexOne];
+            this.data[indexOne] = current;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var currentElement in this.data)
+            {
+                stringBuilder.AppendLine($"{currentElement.GetType()}: {currentElement}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                box.Add(input);
+            }
+
+            int[] swapIndexes = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            box.Swap(swapIndexes[0], swapIndexes[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
