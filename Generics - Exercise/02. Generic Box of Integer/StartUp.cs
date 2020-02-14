fd0d32e0ca91c0ce77ee9475ef06795811f7e4d9
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-14 11:36:32 +0200

    Generic Box of Integer

---
+++ Generics - Exercise/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
