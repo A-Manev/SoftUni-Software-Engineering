79e50a0cbafd8a65345f7a1c54c9bffb31235fd8
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-15 22:53:40 +0200

    Generic Count Method Double

---
+++ Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box()
+        {
+            this.Values = new List<T>();
+        }
+
+        public List<T> Values { get; set; }
+
+        public int CountGreaterElements(T elementToCompare)
+        {
+            int counter = 0;
+
+            foreach (var element in Values)
+            {
+                if (element.CompareTo(elementToCompare) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<double> box = new Box<double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                box.Values.Add(input);
+            }
+
+            double elementToCompare = double.Parse(Console.ReadLine());
+
+            int result = box.CountGreaterElements(elementToCompare);
+
+            Console.WriteLine(result);
+        }
+    }
+}
