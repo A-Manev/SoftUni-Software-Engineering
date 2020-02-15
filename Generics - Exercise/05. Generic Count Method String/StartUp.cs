d4a77af0b8d71d24e890d1b05516cd6ed5e63663
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-15 22:53:07 +0200

    Generic Count Method String

---
+++ Generics - Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<string> box = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Values.Add(input);
+            }
+
+            string elementToCompare = Console.ReadLine();
+
+            int result = box.CountGreaterElements(elementToCompare);
+
+            Console.WriteLine(result);
+        }
+    }
+}
