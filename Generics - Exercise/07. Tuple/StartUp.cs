e8491e6ee587eb163d2b36057ed37b0e579f573d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-15 22:54:15 +0200

    Tuple

---
+++ Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] firstInputInfo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string fullName = $"{firstInputInfo[0]} {firstInputInfo[1]}";
+            string address = firstInputInfo[2];
+
+            string[] secondInputInfo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string name = secondInputInfo[0];
+            int amountOfBeer = int.Parse(secondInputInfo[1]);
+
+            string[] thirdInputInfo = Console.ReadLine()
+               .Split()
+               .ToArray();
+
+            int integerNumber = int.Parse(thirdInputInfo[0]);
+            double doubleNumber = double.Parse(thirdInputInfo[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, address);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, amountOfBeer);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(integerNumber, doubleNumber);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,21 @@
+﻿namespace Tuple
+{
+    public class Tuple<TFirst,TSecond>
+    {
+        public Tuple(TFirst firstItem, TSecond secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+
+        public TFirst FirstItem { get; set; }
+
+        public TSecond SecondItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem}";
+        }
+    }
+}
+ 
