a7f6131c3f2d54233757fac0c84e91f744cb10dc
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-18 17:03:35 +0200

    Heroes

---
+++ CSharp Advanced - Exams/07. CSharp Advanced Exam - 24 February 2019/03. Heroes/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Heroes
+{
+    public class Hero
+    {
+        public Hero(string name, int level, Item item)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Item = item;
+        }
+
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public Item Item { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Hero: {this.Name} – {this.Level}lvl")
+                .AppendLine(this.Item.ToString());
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp Advanced - Exams/07. CSharp Advanced Exam - 24 February 2019/03. Heroes/HeroRepository.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            this.data = new List<Hero>();
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(Hero hero)
+        {
+            this.data.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            var target = data.FirstOrDefault(x => x.Name == name);
+
+            data.Remove(target);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            Hero heroWithHighestStrength = null;
+
+            int maxStrength = int.MinValue;
+
+            foreach (var hero in this.data)
+            {
+                if (hero.Item.Strength > maxStrength)
+                {
+                    maxStrength = hero.Item.Strength;
+                    heroWithHighestStrength = hero;
+                }
+            }
+
+            return heroWithHighestStrength;
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            Hero heroWithHighestAbility = null;
+
+            int maxAbility = int.MinValue;
+
+            foreach (var hero in this.data)
+            {
+                if (hero.Item.Ability > maxAbility)
+                {
+                    maxAbility = hero.Item.Ability;
+                    heroWithHighestAbility = hero;
+                }
+            }
+
+            return heroWithHighestAbility;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            Hero heroWithHighestIntelligence = null;
+
+            int maxIntelligence = int.MinValue;
+
+            foreach (var hero in this.data)
+            {
+                if (hero.Item.Intelligence > maxIntelligence)
+                {
+                    maxIntelligence = hero.Item.Intelligence;
+                    heroWithHighestIntelligence = hero;
+                }
+            }
+
+            return heroWithHighestIntelligence;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in this.data)
+            {
+                stringBuilder.AppendLine(item.ToString());
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
