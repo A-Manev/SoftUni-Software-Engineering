0363995685e6563ae8ea9837de51d8bfa4a15737
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-18 17:04:30 +0200

    Club Party

---
+++ CSharp Advanced - Exams/07. CSharp Advanced Exam - 24 February 2019/01. Club Party/StartUp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClubParty
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int capacity = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Stack<string> stack = new Stack<string>(input);
+
+            Queue<int> reservations = new Queue<int>();
+            Queue<string> rooms = new Queue<string>();
+
+            Dictionary<string, List<int>> halls = new Dictionary<string, List<int>>();
+
+            while (stack.Any())
+            {
+                int reservationNumber;
+                string @string = stack.Pop();
+
+                bool success = int.TryParse(@string, out reservationNumber);
+
+                if (success && rooms.Count == 0)
+                {
+                    continue;
+                }
+
+                if (success)
+                {
+                    reservations.Enqueue(reservationNumber);
+                }
+                else
+                {
+                    rooms.Enqueue(@string);
+                    halls.Add(@string, new List<int>());
+                }
+
+                while (reservations.Any() && rooms.Any())
+                {
+                    int currentReservation = reservations.Peek();
+                    string currentRoom = rooms.Peek();
+
+                    if (halls[currentRoom].Sum() + currentReservation <= capacity)
+                    {
+                        halls[currentRoom].Add(currentReservation);
+                        reservations.Dequeue();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{currentRoom} -> { string.Join(", ", halls[currentRoom])}");
+
+                        rooms.Dequeue();
+                        halls.Remove(currentRoom);
+
+                        if (rooms.Any())
+                        {
+                            string currentRoomB = rooms.Peek();
+
+                            if (halls[currentRoomB].Sum() + currentReservation <= capacity)
+                            {
+                                halls[currentRoomB].Add(currentReservation);
+                                reservations.Dequeue();
+                            }
+                        }
+                        else
+                        {
+                            reservations.Dequeue();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
