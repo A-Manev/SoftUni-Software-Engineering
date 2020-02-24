4ef303757f133881b6592612afabe13e708ce78a
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-24 22:16:58 +0200

    Guild

---
+++ CSharp Advanced - Exams/08. CSharp Advanced Exam - 22 February 2020/03. Guild/Guild.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (this.roster.Count + 1 <= this.Capacity)
+            {
+                this.roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            var target = this.roster.FirstOrDefault(x => x.Name == name);
+
+            if (target != null)
+            {
+                this.roster.Remove(target);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            var target = this.roster.First(x => x.Name == name);
+
+            if (target.Rank != "Member")
+            {
+                target.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            var target = this.roster.First(x => x.Name == name);
+
+            if (target.Rank != "Trial")
+            {
+                target.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] players = this.roster.Where(x => x.Class == @class).ToArray();
+
+            this.roster.RemoveAll(x => x.Class == @class);
+
+            return players;
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Players in the guild: {this.Name}");
+
+            foreach (var player in this.roster)
+            {
+                stringBuilder.AppendLine(player.ToString());
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp Advanced - Exams/08. CSharp Advanced Exam - 22 February 2020/03. Guild/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.Class = @class;
+        }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Rank { get; set; } = "Trial";
+
+        public string Description { get; set; } = "n/a";
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder
+                .AppendLine($"Player {this.Name}: {this.Class}")
+                .AppendLine($"Rank: {this.Rank}")
+                .AppendLine($"Description: {this.Description}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
