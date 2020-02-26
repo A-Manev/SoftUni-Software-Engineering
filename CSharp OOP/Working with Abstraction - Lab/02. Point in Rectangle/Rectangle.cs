9e70c549c0b7eb29254314f169c1617e1996b383
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-26 15:17:46 +0200

    Point in Rectangle

---
+++ CSharp OOP/Working with Abstraction - Lab/02. Point in Rectangle/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            bool isXInside = point.X >= this.TopLeft.X && point.X <= this.BottomRight.X;
+            bool isYInside = point.Y <= this.TopLeft.Y && point.Y >= this.BottomRight.Y;
+
+            return isXInside && isYInside;
+        }
+    }
+}
---
+++ CSharp OOP/Working with Abstraction - Lab/02. Point in Rectangle/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace PointInRectangle
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] coordinates = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int topLeftX = coordinates[0];
+            int topLeftY = coordinates[1];
+            int bottomRightX = coordinates[2];
+            int bottomRightY = coordinates[3];
+
+            Rectangle rectangle = new Rectangle(
+                new Point(topLeftX, bottomRightY), 
+                new Point(bottomRightX, topLeftY));
+
+            int numberOfPoints = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                int[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                int x = input[0];
+                int y = input[1];
+
+                Point point = new Point(x, y);
+
+                Console.WriteLine(rectangle.Contains(point));
+            }
+        }
+    }
+}
