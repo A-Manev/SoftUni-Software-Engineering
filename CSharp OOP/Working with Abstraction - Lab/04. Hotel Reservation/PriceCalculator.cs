07f32ebee2516df2a91228298cd5697249c2706a
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-27 19:53:05 +0200

    Hotel Reservation

---
+++ CSharp OOP/Working with Abstraction - Lab/04. Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelReservation
+{
+    public class PriceCalculator
+    {
+        public decimal GetTotalPrice(decimal pricePerDay, int numberOfDays, Season season, Discount discountType)
+        {
+            decimal totalPrice = pricePerDay * numberOfDays * (int)season; ;
+
+            totalPrice -= totalPrice * (int)discountType / 100;
+
+            return totalPrice;
+        }
+    }
+}
---
+++ CSharp OOP/Working with Abstraction - Lab/04. Hotel Reservation/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace HotelReservation
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            decimal pricePerDay = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            Season season = (Season)Enum.Parse(typeof(Season), input[2]);
+            Discount discountType = Discount.None;
+
+            if (input.Length == 4)
+            {
+                discountType = (Discount)Enum.Parse(typeof(Discount), input[3]);
+            }
+
+            PriceCalculator priceCalculator = new PriceCalculator();
+
+            decimal totalPrice = priceCalculator.GetTotalPrice(pricePerDay, numberOfDays, season, discountType);
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
