3d111e4a2d2f8b024ec53553d1f9af0e51c4cfeb
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-02-28 12:15:40 +0200

    Car Salesman

---
+++ CSharp OOP/Working with Abstraction - Exercise/02. Car Salesman/Engine.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace CarSalesman
+{
+    public class Engine
+    {
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+
+        public Engine(string model, int power, int? displacement)
+            : this(model, power)
+        {
+            this.Displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency)
+            : this(model, power)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int? displacement, string efficiency)
+            : this(model, power, displacement)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public string Model { get; set; }
+
+        public int Power { get; set; }
+
+        public int? Displacement { get; set; }
+
+        public string Efficiency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string displacement = this.Displacement.HasValue
+                ? this.Displacement.ToString()
+                : "n/a";
+
+            string efficiency = string.IsNullOrEmpty(this.Efficiency)
+                ? "n/a"
+                : this.Efficiency;
+
+            stringBuilder
+                .AppendLine($"{this.Model}:")
+                .AppendLine($"    Power: {this.Power}")
+                .AppendLine($"    Displacement: {displacement}")
+                .AppendLine($"    Efficiency: {efficiency}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp OOP/Working with Abstraction - Exercise/02. Car Salesman/StartUp.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarSalesman
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Car> cars = new List<Car>();
+            List<Engine> engines = new List<Engine>();
+
+            int numberOfEngines = int.Parse(Console.ReadLine());
+            AddEngine(engines, numberOfEngines);
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            AddCar(cars, engines, numberOfCars);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+
+        private static void AddCar(List<Car> cars, List<Engine> engines, int numberOfCars)
+        {
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] inputCar = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = inputCar[0];
+                string engineModel = inputCar[1];
+
+                Car car = null;
+
+                Engine engine = engines.First(x => x.Model == engineModel);
+
+                double weight;
+
+                if (inputCar.Length == 2)
+                {
+                    car = new Car(model, engine);
+                }
+                else if (inputCar.Length == 3)
+                {
+                    bool success = double.TryParse(inputCar[2], out weight);
+
+                    car = success
+                        ? new Car(model, engine, weight)
+                        : new Car(model, engine, inputCar[2]);
+                }
+                else if (inputCar.Length == 4)
+                {
+                    weight = double.Parse(inputCar[2]);
+                    string color = inputCar[3];
+
+                    car = new Car(model, engine, weight, color);
+                }
+
+                cars.Add(car);
+            }
+        }
+
+        private static void AddEngine(List<Engine> engines, int numberOfEngines)
+        {
+            for (int i = 0; i < numberOfEngines; i++)
+            {
+                string[] inputEngine = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = inputEngine[0];
+                int power = int.Parse(inputEngine[1]);
+
+                Engine engine = null;
+
+                int displacement;
+
+                if (inputEngine.Length == 2)
+                {
+                    engine = new Engine(model, power);
+                }
+                else if (inputEngine.Length == 3)
+                {
+                    bool success = int.TryParse(inputEngine[2], out displacement);
+
+                    engine = success
+                        ? new Engine(model, power, displacement)
+                        : new Engine(model, power, inputEngine[2]);
+
+                }
+                else if (inputEngine.Length == 4)
+                {
+                    displacement = int.Parse(inputEngine[2]);
+                    string efficiency = inputEngine[3];
+
+                    engine = new Engine(model, power, displacement, efficiency);
+                }
+
+                engines.Add(engine);
+            }
+        }
+    }
+}
