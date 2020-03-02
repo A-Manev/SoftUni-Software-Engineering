35b007935469e4f2d091aaf841575a0f43813261
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-02 22:02:41 +0200

    Random List

---
+++ CSharp OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public RandomList()
+            : base()
+        {
+
+        }
+
+        public string RandomString()
+        {
+            Random random = new Random();
+
+            int index = random.Next(0, base.Count);
+            string element = base[index];
+            base.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
