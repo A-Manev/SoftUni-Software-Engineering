19bfb8f7d566b087b317bdd6965b659a3ff0acfe
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-02 22:02:15 +0200

    Hierarchical Inheritance

---
+++ CSharp OOP/Inheritance - Lab/03. Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
---
+++ CSharp OOP/Inheritance - Lab/03. Hierarchical Inheritance/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public Cat()
+            : base()
+        {
+
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("meowing…");
+        }
+    }
+}
