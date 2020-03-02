5fda912b8f76e07a6445d752bd2efb4cf1c32e6a
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-02 22:03:21 +0200

    Stack of Strings

---
+++ CSharp OOP/Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public StackOfStrings()
+            : base()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+            if (base.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+
+                base.Push(element);
+        }
+    }
+}
