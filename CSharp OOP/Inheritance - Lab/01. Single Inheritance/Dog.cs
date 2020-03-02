7ccbefcec6e2fbda9bf109f8d944a21027fc4118
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-02 22:00:58 +0200

    Single Inheritance

---
+++ CSharp OOP/Inheritance - Lab/01. Single Inheritance/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public Dog()
+        : base()
+        {
+
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("barking…");
+        }
+    }
+}
