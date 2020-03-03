bb5bb30cb3d50aed307c1a6c99686973a166278d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-03 20:26:34 +0200

    Person

---
+++ CSharp OOP/Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private const int PERSON_MIN_AGE = 0;
+        
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value >= PERSON_MIN_AGE)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
