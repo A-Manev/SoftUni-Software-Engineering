a64c604a4874cd52a5b46e429594da457421d2e4
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-03 20:30:50 +0200

    Need for Speed

---
+++ CSharp OOP/Inheritance - Exercise/04. Need for Speed/Cars/SportCar.cs
@@ -0,0 +1,15 @@
+﻿namespace NeedForSpeed.Cars
+{
+    public class SportCar : Car
+    {
+        private const double DEFAULT_FUEL_CONSUMPTION = 10;
+
+        public SportCar(int horsePower, double fuel) 
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        public override double FuelConsumption => DEFAULT_FUEL_CONSUMPTION;
+    }
+}
---
+++ CSharp OOP/Inheritance - Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DEFAULT_FUEL_CONSUMPTION = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DEFAULT_FUEL_CONSUMPTION;
+
+        public virtual void Drive(double kilometers)
+        {
+            double consumptedFuel = kilometers * this.FuelConsumption;
+
+            if (this.Fuel >= consumptedFuel)
+            {
+                this.Fuel -= consumptedFuel;
+            }
+        }
+    }
+}
