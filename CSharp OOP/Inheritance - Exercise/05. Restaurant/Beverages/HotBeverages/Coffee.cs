eb77838ada1a09196322666c6555ab4eeb0dc0e2
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-04 14:53:17 +0200

    Restaurant

---
+++ CSharp OOP/Inheritance - Exercise/05. Restaurant/Beverages/HotBeverages/Coffee.cs
@@ -0,0 +1,17 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal COFFEE_PRICE = 3.50m;
+
+        private const double COFFEE_MILLILITRES = 50;
+
+        public Coffee(string name, double caffeine) 
+            : base(name, COFFEE_PRICE, COFFEE_MILLILITRES)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine  { get; set; }
+    }
+}
