804ed67e5bb987fc2e7920fe71b91f4eb1e8ab7c
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-05 22:04:19 +0200

    Team

---
+++ CSharp OOP/Encapsulation - Lab/04. Team/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] commandArguments = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                try
+                {
+                    string firstName = commandArguments[0];
+                    string lastName = commandArguments[1];
+                    int age = int.Parse(commandArguments[2]);
+                    decimal salary = decimal.Parse(commandArguments[3]);
+
+                    Person person = new Person(firstName, lastName, age, salary);
+                    people.Add(person);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
