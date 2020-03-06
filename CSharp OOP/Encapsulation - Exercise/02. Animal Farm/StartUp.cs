4db010ec8abc7a33d993f199b140119e96e4eb5d
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-06 21:48:53 +0200

    Animal Farm

---
+++ CSharp OOP/Encapsulation - Exercise/02. Animal Farm/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AnimalFarm
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(chicken);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
