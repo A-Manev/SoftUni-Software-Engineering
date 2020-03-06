54d910e9b5a448ff753894e3e64c66ce2b1ebf3b
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-06 21:48:25 +0200

    Class Box Data

---
+++ CSharp OOP/Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            this.Length = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                ValidationSide(value, nameof(Length));
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                ValidationSide(value, nameof(Width));
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                ValidationSide(value, nameof(Height));
+                this.height = value;
+            }
+        }
+
+        private void ValidationSide(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+
+        public double CalculeteSurfaceArea()
+        {
+            return (2 * this.length * this.width) + this.CalculeteLateralSurfaceArea();
+        }
+
+        public double CalculeteLateralSurfaceArea()
+        {
+            return 2 * (this.length * this.height + this.width * this.height);
+        }
+
+        public double CalculeteVolume()
+        {
+            return this.length * this.width * this.height;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder
+                .AppendLine($"Surface Area - {this.CalculeteSurfaceArea():F2}")
+                .AppendLine($"Lateral Surface Area - {this.CalculeteLateralSurfaceArea():F2}")
+                .AppendLine($"Volume - {this.CalculeteVolume():F2}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp OOP/Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, width, height);
+                Console.WriteLine(box);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
