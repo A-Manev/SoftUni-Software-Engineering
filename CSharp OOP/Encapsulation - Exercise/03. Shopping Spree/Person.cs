c97932c20f8d4326e6ee79b0684f0743fc5d6514
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-06 21:49:34 +0200

    Shopping Spree

---
+++ CSharp OOP/Encapsulation - Exercise/03. Shopping Spree/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products 
+        {
+            get
+            {
+                return this.products;
+            } 
+        }
+
+        public bool BuyProduct(Product product) 
+        {
+            if (this.money >= product.Cost)
+            {
+                this.money -= product.Cost;
+                this.products.Add(product);
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return this.Products.Count > 0
+                ? $"{this.Name} - {string.Join(", ", this.Products)}"
+                : $"{this.Name} - Nothing bought ";
+        }
+    }
+}
---
+++ CSharp OOP/Encapsulation - Exercise/03. Shopping Spree/StartUp.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            string[] inputPeople = Console.ReadLine()
+                .Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] inputProducts = Console.ReadLine()
+                .Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < inputPeople.Length; i++)
+            {
+                string[] peopleArguments = inputPeople[i]
+                    .Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = peopleArguments[0];
+                decimal money = decimal.Parse(peopleArguments[1]);
+
+                try
+                {
+                    Person person = new Person(name, money);
+                    people.Add(person);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < inputProducts.Length; i++)
+            {
+                string[] productsArguments = inputProducts[i]
+                    .Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = productsArguments[0];
+                decimal cost = decimal.Parse(productsArguments[1]);
+
+                try
+                {
+                    Product product = new Product(name, cost);
+                    products.Add(product);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    return;
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] commandArguments = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string personName = commandArguments[0];
+                string productName = commandArguments[1];
+
+                Person currentPerson = people.FirstOrDefault(x => x.Name == personName);
+                Product currentProduct = products.FirstOrDefault(x => x.Name == productName);
+
+                if (currentPerson.BuyProduct(currentProduct))
+                {
+                    Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
