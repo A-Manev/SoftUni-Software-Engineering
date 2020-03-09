5866f0e601cd4bce7b3a8616766cffda98dd90f0
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-09 10:51:10 +0200

    Validation

---
+++ CSharp OOP/Encapsulation - Lab/03. Validation/Person.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private const int MINIMUM_NAME_LENGTH = 3;
+        private const decimal MINIMUN_SALARY = 460;
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < MINIMUM_NAME_LENGTH)
+                {
+                    throw new ArgumentException($"First name cannot contain fewer than {MINIMUM_NAME_LENGTH} symbols!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < MINIMUM_NAME_LENGTH)
+                {
+                    throw new ArgumentException($"Last name cannot contain fewer than {MINIMUM_NAME_LENGTH} symbols!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public decimal Salary 
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < MINIMUN_SALARY)
+                {
+                    throw new ArgumentException($"Salary cannot be less than {MINIMUN_SALARY} leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.Salary *= 1 + (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:F2} leva.";
+        }
+    }
+}
---
+++ CSharp OOP/Encapsulation - Lab/03. Validation/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] commandArguments = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                try
+                {
+                    string firstName = commandArguments[0];
+                    string lastName = commandArguments[1];
+                    int age = int.Parse(commandArguments[2]);
+                    decimal salary = decimal.Parse(commandArguments[3]);
+
+                    Person person = new Person(firstName, lastName, age, salary);
+
+                    people.Add(person);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            IncreasePeopleSalary(people, percentage);
+
+            PrintPeople(people);
+        }
+
+        private static void IncreasePeopleSalary(List<Person> people, decimal percentage)
+        {
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percentage);
+            }
+        }
+
+        private static void PrintPeople(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
