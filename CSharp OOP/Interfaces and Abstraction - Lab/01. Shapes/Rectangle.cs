c90b0d70083d7a97354d2e0e778255802204088b
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-10 13:47:26 +0200

    Shapes

---
+++ CSharp OOP/Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(double width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+
+            Console.WriteLine(end);
+        }
+
+        //public void Draw()
+        //{
+        //    Console.WriteLine(new string('*', this.Width));
+
+        //    for (int i = 0; i < this.Height - 2; i++)
+        //    {
+        //        Console.WriteLine("*" + new string(' ', this.Width - 2) + "*");
+        //    }
+
+        //    Console.WriteLine(new string('*', this.Width));
+        //}
+    }
+}
---
+++ CSharp OOP/Interfaces and Abstraction - Lab/01. Shapes/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double radius = double.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(5);
+
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            IDrawable rectangle = new Rectangle(5, 10);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
