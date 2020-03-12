0eae5d6cda302862bd686d3a4c2ce0473874fc71
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-12 20:38:18 +0200

    Define an Interface IPerson

---
+++ CSharp OOP/Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            IPerson person = new Citizen(name, age);
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
