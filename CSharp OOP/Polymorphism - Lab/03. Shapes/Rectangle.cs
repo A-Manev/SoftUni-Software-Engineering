b044dc2466b3f0d047b966ef23eabeb2c1c4560b
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-15 22:48:57 +0200

    Shapes

---
+++ CSharp OOP/Polymorphism - Lab/03. Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get => this.height;
+            set => this.height = value;
+        }
+
+        public double Width 
+        {
+            get => this.width;
+            set => this.width = value; 
+        }
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
