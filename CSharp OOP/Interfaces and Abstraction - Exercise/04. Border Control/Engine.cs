7e51560ef9e8ab6a0a7b5cd781d01f16b2936a21
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-20 23:06:36 +0200

    Border Control

---
+++ CSharp OOP/Interfaces and Abstraction - Exercise/04. Border Control/Engine.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+    class Engine
+    {
+        private List<IIdentifiable> populations;
+
+        public Engine()
+        {
+            this.populations = new List<IIdentifiable>();
+        }
+
+        public void Run()
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandArguments = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (commandArguments.Length == 3)
+                {
+                    this.AddCitizen(commandArguments);
+                }
+                else
+                {
+                    this.AddRobot(commandArguments);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            PrintFakeIds();
+        }
+
+        private void AddCitizen( string[] commandArguments)
+        {
+            string name = commandArguments[0];
+            int age = int.Parse(commandArguments[1]);
+            string id = commandArguments[2];
+
+            Citizen citizen = new Citizen(name, age, id);
+
+            this.populations.Add(citizen);
+        }
+
+        private void AddRobot(string[] commandArguments)
+        {
+            string model = commandArguments[0];
+            string id = commandArguments[1];
+
+            Robot robot = new Robot(model, id);
+
+            this.populations.Add(robot);
+        }
+
+        private void PrintFakeIds()
+        {
+            string fakeId = Console.ReadLine();
+
+            List<string> allFakeIds = this.populations
+                .Where(x => x.Id.EndsWith(fakeId))
+                .Select(x => x.Id)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, allFakeIds));
+        }
+    }
+}
