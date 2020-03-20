3ffb217d433befb355d9c19bdcce4cddd076e21e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-20 23:11:28 +0200

    Explicit Iterfaces

---
+++ CSharp OOP/Interfaces and Abstraction - Exercise/09. Explicit Interfaces/Engine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+using ExplicitInterfaces.Interfaces;
+using ExplicitInterfaces.Models;
+
+namespace ExplicitInterfaces
+{
+    class Engine
+    {
+        public Engine()
+        {
+
+        }
+
+        public void Run()
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+
+                string[] commandArguments = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                this.PrintOutput(commandArguments);
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private void PrintOutput(string[] commandArguments)
+        {
+            string name = commandArguments[0];
+            string country = commandArguments[1];
+            int age = int.Parse(commandArguments[2]);
+
+            Citizen citizen = new Citizen(name, age, country);
+
+            Type type = citizen.GetType();
+
+            IResident iResidentCitizen = (IResident)Convert.ChangeType(citizen, type);
+
+            Console.WriteLine(citizen.GetName());
+            Console.WriteLine(iResidentCitizen.GetName());
+        }
+    }
+}
---
+++ CSharp OOP/Interfaces and Abstraction - Exercise/09. Explicit Interfaces/Models/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using ExplicitInterfaces.Interfaces;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
