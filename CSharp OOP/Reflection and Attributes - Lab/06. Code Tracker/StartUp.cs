749bb685dbaec0ec176b55fb467f748d99a1124e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-03-25 15:53:07 +0200

    Code Tracker

---
+++ CSharp OOP/Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,11 @@
+﻿[Author("Ventsi")]
+public class StartUp
+{
+    [Author("Gosho")]
+    public static void Main()
+    {
+        Tracker tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
+
---
+++ CSharp OOP/Reflection and Attributes - Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(StartUp);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+        foreach (MethodInfo method in methods)
+        {
+            if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+            {
+                object[] attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute author in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {author.Name}");
+                }
+            }
+        }
+    }
+}
