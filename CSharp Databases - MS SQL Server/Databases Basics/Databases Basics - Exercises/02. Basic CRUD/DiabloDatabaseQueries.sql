39d0c7c3b53ceaedd213f372e2879974821ccde3
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-05-26 18:38:38 +0300

    Diablo Database Queries

---
+++ CSharp Databases - MS SQL Server/Databases Basics/Databases Basics - Exercises/02. Basic CRUD/DiabloDatabaseQueries.sql
@@ -0,0 +1,9 @@
+USE Diablo
+
+-- Part III – Queries for Diablo Database
+
+-- 25. All Diablo Characters
+-- Display all characters in alphabetical order. 
+
+SELECT [Name] FROM [Characters] 
+	ORDER BY [Name] ASC
