9cd7332c5bdbf2a6b647a0f5c695dec24c8ebf0e
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-05-27 19:41:42 +0300

    University Database

---
+++ CSharp Databases - MS SQL Server/Databases Basics/Databases Basics - Exercises/03. Table Relations/UniversityDatabase.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE University 
+
+USE University 
+
+CREATE TABLE Majors(
+	[MajorID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students(
+	[StudentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[StudentNumber] NVARCHAR(50) NOT NULL,
+	[StudentName] NVARCHAR(50) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES Majors([MajorID])
+)
+
+CREATE TABLE Payments(
+	[PaymentID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[PaymentDate] DATETIME2 NOT NULL,
+	[PaymentAmount] NVARCHAR(50) NOT NULL,
+	[StudentID] INT FOREIGN KEY REFERENCES Students([StudentID])
+)
+
+CREATE TABLE Subjects(
+	[SubjectID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[SubjectName] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Agenda(
+	[StudentID] INT FOREIGN KEY REFERENCES Students([StudentID]),
+	[SubjectID] INT FOREIGN KEY REFERENCES Subjects([SubjectID])
+	PRIMARY KEY ([StudentID], [SubjectID])
+)
