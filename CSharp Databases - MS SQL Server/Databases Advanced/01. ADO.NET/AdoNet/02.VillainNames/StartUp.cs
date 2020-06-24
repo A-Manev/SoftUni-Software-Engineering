2ed152209dcbc3c13e055caf336de7cca4d88f75
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-06-24 22:10:58 +0300

    Villain Names

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/01. ADO.NET/AdoNet/02.VillainNames/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace VillainNames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string connectionString = "Server=.\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                string command = @"SELECT *
+	                                    FROM (
+	                                    	  SELECT Villains.Name,
+	                                    	         COUNT(*) AS [MinionsCount]
+	                                    	      FROM Villains
+	                                    	 	  JOIN MinionsVillains ON Villains.Id = MinionsVillains.VillainId
+	                                    	      GROUP BY Villains.Name
+	                                    	 ) AS TEMP
+	                                    WHERE [MinionsCount] > 3
+	                                    ORDER BY [MinionsCount] DESC";
+
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string villainName = (string)reader["Name"];
+                    int numberOfMinions = (int)reader["MinionsCount"];
+
+                    Console.WriteLine(villainName + " - " + numberOfMinions);
+                }
+            }
+        }
+    }
+}
