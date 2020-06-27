39d6514424c58ac944ef2d903bc9f485f7804d19
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-06-27 23:30:00 +0300

    Print All Minion Names

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/01. ADO.NET/AdoNet/07.PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace PrintAllMinionNames
+{
+    class StartUp
+    {
+        private static string connectionString = "Server=.\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+
+        static void Main()
+        {
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            string result = PrintAllMinionNames(sqlConnection);
+
+            Console.WriteLine(result);
+        }
+
+        private static string PrintAllMinionNames(SqlConnection sqlConnection)
+        {
+            StringBuilder output = new StringBuilder();
+
+            List<string> allMinionNames = new List<string>();
+
+            string selectAllMinionsQuery = @"SELECT Name FROM Minions";
+
+            using SqlCommand selectAllMinionsCommand = new SqlCommand(selectAllMinionsQuery, sqlConnection);
+
+            using SqlDataReader reader = selectAllMinionsCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string minionName = (string)reader["Name"];
+                allMinionNames.Add(minionName);
+            }
+
+            if (allMinionNames.Count % 2 == 0)
+            {
+                ReturnNewMinionArrangement(output, allMinionNames);
+            }
+            else
+            {
+                ReturnNewMinionArrangement(output, allMinionNames);
+
+                double midPerson = Math.Floor(allMinionNames.Count / 2.0);
+                output.AppendLine(allMinionNames[(int)midPerson]);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        private static void ReturnNewMinionArrangement(StringBuilder output, List<string> allMinionNames)
+        {
+            for (int i = 0; i < allMinionNames.Count / 2; i++)
+            {
+                output.AppendLine(allMinionNames[i]);
+                output.AppendLine(allMinionNames[allMinionNames.Count - i - 1]);
+            }
+        }
+    }
+}
