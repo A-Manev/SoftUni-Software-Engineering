f29e96c55e14aff9e0647181f5c86e14064ad106
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-07-06 22:20:47 +0300

    FootballBetting startup class

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/04. Entity Relations/EntityRelations/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new FootballBettingContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
