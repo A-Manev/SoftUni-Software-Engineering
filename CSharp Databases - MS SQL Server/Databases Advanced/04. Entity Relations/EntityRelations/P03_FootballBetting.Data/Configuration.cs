12358558cc3a4cc5b839c2d50ae3ddf46e120326
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-07-07 13:09:40 +0300

    FootballBetting data

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/04. Entity Relations/EntityRelations/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    internal static class Configuration
+    {
+        internal static string ConnectionString =
+           @"Server=.\SQLEXPRESS;Database=FootballBettingSystem;Integrated Security=True";
+    }
+}
