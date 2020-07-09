e519edf2f277bff605304cc522b95b59ea620ba0
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-07-09 16:47:32 +0300

    HospitalDatabase startup

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/05. Code-First/CodeFirst/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using P01_HospitalDatabase.Data;
+using System;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+       public  static void Main()
+        {
+            //using var context = new HospitalContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+        }
+    }
+}
