d6fd0e5d9e570d209d434562eaa2982ea326324f
A-Manev <52919486+A-Manev@users.noreply.github.com>
2020-07-09 16:48:37 +0300

    HospitalDatabase data models

---
+++ CSharp Databases - MS SQL Server/Databases Advanced/05. Code-First/CodeFirst/P01_HospitalDatabase.Data.Models/Medicament.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Medicament
+    {
+        public Medicament()
+        {
+            this.Prescriptions = new HashSet<PatientMedicament>();
+        }
+
+        public int MedicamentId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
---
+++ CSharp Databases - MS SQL Server/Databases Advanced/05. Code-First/CodeFirst/P01_HospitalDatabase.Data.Models/Patient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            this.Prescriptions = new HashSet<PatientMedicament>();
+
+            this.Visitations = new HashSet<Visitation>();
+
+            this.Diagnoses = new HashSet<Diagnose>();
+        }
+
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+
+        public virtual ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
